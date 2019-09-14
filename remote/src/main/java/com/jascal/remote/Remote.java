67e9f983407e8e71b6c0a82cce5971b936b2dba1
no.47 <jascal@163.com>
2019-09-14 23:22:33 +0800

    init remote module
    on branch: master
    
    with module: remote
    
    describe: init api module

---
+++ remote/src/main/java/com/jascal/remote/Remote.java
@@ -0,0 +1,46 @@
+package com.jascal.remote;
+
+/**
+ * @author ihave4cat
+ * @describe TODO
+ * @data on 2019-09-14 23:19
+ * @email jascal@163.com
+ */
+public class Remote {
+    /**
+     * {
+     *   "login": "MashirosBaumkuchen",
+     *   "id": 12267364,
+     *   "node_id": "MDQ6VXNlcjEyMjY3MzY0",
+     *   "avatar_url": "https://avatars0.githubusercontent.com/u/12267364?v=4",
+     *   "gravatar_id": "",
+     *   "url": "https://api.github.com/users/MashirosBaumkuchen",
+     *   "html_url": "https://github.com/MashirosBaumkuchen",
+     *   "followers_url": "https://api.github.com/users/MashirosBaumkuchen/followers",
+     *   "following_url": "https://api.github.com/users/MashirosBaumkuchen/following{/other_user}",
+     *   "gists_url": "https://api.github.com/users/MashirosBaumkuchen/gists{/gist_id}",
+     *   "starred_url": "https://api.github.com/users/MashirosBaumkuchen/starred{/owner}{/repo}",
+     *   "subscriptions_url": "https://api.github.com/users/MashirosBaumkuchen/subscriptions",
+     *   "organizations_url": "https://api.github.com/users/MashirosBaumkuchen/orgs",
+     *   "repos_url": "https://api.github.com/users/MashirosBaumkuchen/repos",
+     *   "events_url": "https://api.github.com/users/MashirosBaumkuchen/events{/privacy}",
+     *   "received_events_url": "https://api.github.com/users/MashirosBaumkuchen/received_events",
+     *   "type": "User",
+     *   "site_admin": false,
+     *   "name": "冬",
+     *   "company": null,
+     *   "blog": "https://mashirosbaumkuchen.github.io/",
+     *   "location": null,
+     *   "email": null,
+     *   "hireable": null,
+     *   "bio": "emmm.. interesting",
+     *   "public_repos": 41,
+     *   "public_gists": 0,
+     *   "followers": 7,
+     *   "following": 12,
+     *   "created_at": "2015-05-06T06:03:10Z",
+     *   "updated_at": "2019-08-30T11:48:41Z"
+     * }
+     * */
+
+}
